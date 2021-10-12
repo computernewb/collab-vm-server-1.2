22db52e1cd9a8d091145732b418513dac3c68cda
yellows111 <ice_ice@email.com>
2021-10-12 19:54:47 +0100

    "part 1"

--- scripts/copy_dlls_cyg.sh
+++ scripts/copy_dlls_cyg.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cp cvmlib/bin/*.dll $2
 LIST=$(ldd $2/collab-vm-server.exe | cut -d '>' -f2 | cut -d '(' -f1 | cut -d ' ' -f2 | sed -E '/\/c\//d' | sed -E '/\/home\//d' | sed -E '/not/d')
 echo $LIST
 if [ "$LIST" != "" ]; then cp $LIST $2 2>/dev/null; fi
--- scripts/copy_dlls_mw.sh
+++ scripts/copy_dlls_mw.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ "$1" == "x86" ]; then
-	cp cvmlib32/bin/*.dll $2
-else
-	cp cvmlib/bin/*.dll $2
-fi
-
 LIST=$(ldd $2/collab-vm-server.exe | cut -d '>' -f2 | cut -d '(' -f1 | cut -d ' ' -f2 | sed -E '/\/c\//d' | sed -E '/\/home\//d')
 
 echo $LIST
