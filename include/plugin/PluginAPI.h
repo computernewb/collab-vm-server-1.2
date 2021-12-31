b2513d7386428c3dcae39778e9609e323425373a
modeco80 <lily.modeco80@protonmail.ch>
2021-12-30 19:26:39 -0500

    add usermessage and IUser interfaces

--- include/plugin/PluginAPI.h
+++ include/plugin/PluginAPI.h
@@ -47,6 +47,24 @@ namespace collabvm::plugin {
 	};
 
 	/**
+	 * Interface for CollabVM users.
+	 */
+	struct IUser {
+		enum class Type : std::uint8_t {
+			Unregistered,
+			Registered,
+			Moderator,
+			Admin
+		};
+
+		COLLABVM_PLUGINABI_DEFINE_VTFUNC(IUser, IUserEntry*, GetUserEntry);
+		COLLABVM_PLUGINABI_DEFINE_VTFUNC(IUser, IUser::Type, GetType);
+
+		COLLABVM_PLUGINABI_DEFINE_VTFUNC(IUser, void, Kick, const utf8char* reason /* Optional, pass nullptr for "no reason" */);
+		COLLABVM_PLUGINABI_DEFINE_VTFUNC(IUser, void, Ban, bool isForever, const utf8char* reason /* Optional, pass nullptr for "no reason" */);
+	};
+
+	/**
 	 * Basic plugin interface.
 	 *
 	 * Consumed by coreplugins or regular plugins
@@ -58,6 +76,32 @@ namespace collabvm::plugin {
 		// malloc()/free() which goes into the main CollabVM Server heap.
 		COLLABVM_PLUGINABI_DEFINE_VTFUNC(IPluginApi, void*, Malloc, std::size_t size);
 		COLLABVM_PLUGINABI_DEFINE_VTFUNC(IPluginApi, void, Free, void* ptr);
+
+		// websocket stuff
+
+		/**
+		 * Register a usermessage with the given ID and handler.
+		 * the server will dispatch to this plugin automatically.
+		 *
+		 * \param[in] id The user-message ID.
+		 * \param[in] handler The user-message handler.
+		 */
+		COLLABVM_PLUGINABI_DEFINE_VTFUNC(IPluginApi, std::uint32_t /* "Handle */, RegisterUserMessage, std::uint32_t id, void(*handler)(IPluginApi* plugin, const std::uint8_t* buffer, std::size_t length));
+
+		/**
+		 * Unregister a previously registered (by IPluginApi::RegisterUserMessage()) plugin usermessage.
+		 */
+		COLLABVM_PLUGINABI_DEFINE_VTFUNC(IPluginApi, void, UnregisterUserMessage, std::uint32_t handle);
+
+		/**
+		 * Send a message to an individual CollabVM user.
+		 */
+		COLLABVM_PLUGINABI_DEFINE_VTFUNC(IPluginApi, void, SendMessage, IUser* user, std::uint8_t* buffer, std::size_t buffer_length);
+
+		/**
+		 * Broadcast a message to all CollabVM users.
+		 */
+		COLLABVM_PLUGINABI_DEFINE_VTFUNC(IPluginApi, void, BroadcastMessage, std::uint8_t* buffer, std::size_t buffer_length);
 
 		// TODO: more vtfuncs
 
