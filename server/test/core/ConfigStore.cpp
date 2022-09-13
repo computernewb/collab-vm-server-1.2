e150187f28e1fe3c57e892c5887eb92c5f5254f0
modeco80 <lily.modeco80@protonmail.ch>
2022-09-13 19:50:47 -0400

    server: change ConfigStore tests a bit

--- server/test/core/ConfigStore.cpp
+++ server/test/core/ConfigStore.cpp
@@ -21,23 +21,23 @@ TEST_CASE("ConfigStore value manipulation", "[ConfigStore]") {
 			REQUIRE_THROWS_AS(store["XXXINVALID_KEY"].As<std::int64_t>(), ConfigStore::ArrayProxy::NonExistentValue);
 		}
 
-		AND_THEN("Inserting a value of a given type") {
+		THEN("Inserting a value of a given type") {
 			store["value"].Set(static_cast<std::uint64_t>(32));
 
-			THEN("inserting works as expected") {
+			AND_THEN("inserting works as expected") {
 				REQUIRE(store["value"].Exists());
 				REQUIRE(store["value"].Is<std::uint64_t>());
 			}
 
-			THEN("Looking up a value with the right type succeeds") {
+			AND_THEN("Looking up a value with the right type succeeds") {
 				REQUIRE_NOTHROW(store["value"].As<std::uint64_t>());
 			}
 
-			THEN("Looking up a value with the wrong type fails") {
+			AND_THEN("Looking up a value with the wrong type fails") {
 				REQUIRE_THROWS_AS(store["value"].As<std::string>(), ConfigStore::ArrayProxy::InvalidType);
 			}
 
-			THEN("Removing a existent value works") {
+			AND_THEN("Removing a existent value works") {
 				store["value"].Remove();
 
 				REQUIRE(store["value"].Exists() == false);
