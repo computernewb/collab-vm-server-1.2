641c7f4fde157e333946648e2593ec425a875570
modeco80 <lily.modeco80@protonmail.ch>
2022-12-11 05:33:27 -0500

    server/host: remove old boost.po argument code
    broke build, don't need it, etc, etc.

--- server/host/main.cpp
+++ server/host/main.cpp
@@ -3,25 +3,15 @@
 //
 // (C) 2021-2022 CollabVM Development Team
 //
-// This file is licensed under the GNU General Public License Version 3.
-// Text is provided in LICENSE.
+// SPDX-License-Identifier: GPL-3.0
 //
 
 // Simple host
 
-#include <boost/asio/io_context.hpp>
-#include <boost/asio/signal_set.hpp>
 #include <iostream>
-
-#include "Arguments.hpp"
-
 #include <core/config/ConfigStore.hpp>
 
 int main(int argc, char** argv) {
-	// maybe global instance this so things can access it?
-	//collab3::host::Arguments args;
-	//boost::asio::io_context ioc;
-
 	collab3::core::ConfigStore store;
 
 	// Set some values
@@ -35,21 +25,5 @@ int main(int argc, char** argv) {
 	std::cout << "test2: \"" << store["test2"].As<bool>().value() << "\"\n";
 	std::cout << "test3: \"" << store["test3"].As<std::int64_t>().value() << "\"\n";
 	std::cout << "test4: \"" << store["test4"].As<std::uint64_t>().value() << "\"\n";
-
-#if 0
-	args.Process(argc, argv);
-
-	// Set up Ctrl+C handler
-	boost::asio::signal_set interruptSignal(ioc, SIGINT, SIGTERM);
-	interruptSignal.async_wait([&](const boost::system::error_code& ec, int sig) {
-		std::cout << "Stopping...\n";
-		ioc.stop();
-	});
-
-
-	// Run the io_service on the api thread.
-	ioc.run();
-
-#endif
 	return 0;
 }
